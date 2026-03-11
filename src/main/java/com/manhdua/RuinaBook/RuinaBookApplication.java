124464ef4f7d8a20b0579d0bdbf80cf799f8fc1b
manhdua1 <manhvu20012005@gmail.com>
2026-03-11 23:30:47 +0700

    Setup project

---
+++ src/main/java/com/manhdua/RuinaBook/RuinaBookApplication.java
@@ -0,0 +1,13 @@
+package com.manhdua.RuinaBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuinaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuinaBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manhdua/RuinaBook/RuinaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhdua.RuinaBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuinaBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
